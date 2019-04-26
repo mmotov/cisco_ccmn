574432f42465bd3e9415fa73c96e9ba7fe8afd38
555ukr <kalashnykdmytro@ukr.net>
2019-04-26 09:00:23 +0300

    stange thing happens

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Route, Redirect, BrowserRouter} from 'react-router-dom';
+import {browserHistory} from 'react-router'
 import './App.css';
-import Home from './Home/Home';
-import Analytics from './Analytics/Analytics';
-import Header from './Layout/Header';
+import Home from './Home/Home.jsx';
+import Analytics from './Analytics/Analytics.jsx';
+import Header from './Layout/Header.jsx';
+import Login from './Layout/Login.jsx';
+
+let Auth = (Component) => {
+  if (!localStorage.getItem('cisco_auth')){
+    return () => ( <Redirect to='/login' /> );
+  }
+  console.log(Component)
+  return () => ( <Component /> );
+  // localStorage.setItem('cisco_auth', JSON.stringify({
+  //   password: btoa("123"),
+  //   username: "lol"
+  // }));
+  // if (true){
+  //   console.log(localStorage.getItem('cisco_auth'))
+  //   return () => (
+  //         <ComponentToDebug />
+  //     );
+  // }
+  // console.log("lol")
+  // return () => (
+  //       <ComponentToDebug />
+  //   );
+};
 
 class App extends Component {
+
   render() {
     return (
-      <main>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/analytics' component={Analytics}/>
-        </Switch>
-      </main>
+      <div>
+      <BrowserRouter>
+            <Route path={"/"} component={Header} />
+            <Route exact path={"/"} component={Auth(Home)} />
+            <Route path={"/analytics"} component={Auth(Analytics)} />
+            <Route path={"/login"} component={Login} />
+      </BrowserRouter>
+      </div>
     );
   }
 }
